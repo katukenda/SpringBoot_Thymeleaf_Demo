34429dbfebc394aa8beb672bfa00359551c423a1
katukenda <janithaprashadkatukenda@gmail.com>
2022-08-18 10:44:59 +0530

    with database configuration

---
+++ src/main/java/com/katukenda/loginregistration/RegistrationLoginSpringBootSecurityThymeleafApplication.java
@@ -0,0 +1,17 @@
+package com.katukenda.loginregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class RegistrationLoginSpringBootSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSpringBootSecurityThymeleafApplication.class, args);
+	}
+	
+
+}
